bc6d42fd9751f2b39b9b542099704ab8c56f1651
Emma <hello@prpl.wtf>
2025-12-01 17:15:05 +0100

    feat: more work on site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import './layout.css';
+	import { resolve } from '$app/paths';
 	import favicon from '$lib/assets/favicon.svg';
+	import './layout.css';
 
 	let { children } = $props();
 </script>
@@ -9,18 +10,58 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="p-4 md:p-16 lg:p-24 h-full md:fixed flex flex-col justify-center top-0 left-0">
-	<div>
+<div class="p-4 md:p-16 lg:p-24 fixed top-3 md:top-0 left-0 flex items-center gap-4 z-10 h-10">
+	<a href={resolve('/')} title="Home" class="group">
+		<enhanced:img
+			src="/static/img/circles_drawn.png"
+			alt="Emma's logo"
+			width="40"
+			class="-mt-1 -rotate-2 group-hover:opacity-75 transition-opacity"
+		/>
+	</a>
+	<div class="flex gap-1.5 h-10 w-100">
+		<button
+			class="w-1/3 flex bg-sandrift-200 hover:bg-sandrift-300/60 active:bg-sandrift-300 active:w-[38%] items-center justify-center gap-0.5 transition-all rounded-l-3xl rounded-r-lg"
+		>
+			<span>Projects</span>
+		</button>
+		<button
+			class="w-1/3 flex bg-sandrift-200 hover:bg-sandrift-300/60 active:bg-sandrift-300 active:w-[38%] items-center justify-center gap-0.5 transition-all rounded-lg"
+		>
+			<span>Experience</span>
+		</button>
+		<button
+			class="w-1/3 flex bg-sandrift-200 hover:bg-sandrift-300/60 active:bg-sandrift-300 active:w-[38%] items-center justify-center gap-0.5 transition-all rounded-r-3xl rounded-l-lg"
+		>
+			<span>Contact</span>
+		</button>
+	</div>
+</div>
+
+<div
+	class="p-4 md:p-16 lg:p-24 h-full md:fixed md:mt-0 mt-20 flex flex-col justify-center top-0 left-0"
+>
+	<div class="md:max-w-150 md:w-auto w-full">
 		{@render children()}
 	</div>
 </div>
 
-<div class="fixed -bottom-[50vw] -right-[50vw] -z-10">
-	<div class="rounded-full w-[100vw] h-[100vw] p-[10vw] border border-sandrift-400/20">
-		<div class="rounded-full w-[80vw] h-[80vw] p-[10vw] border border-sandrift-400/40">
-			<div class="rounded-full w-[60vw] h-[60vw] p-[10vw] border border-sandrift-400/60">
-				<div class="rounded-full w-[40vw] h-[40vw] p-[10vw] border border-sandrift-400/80">
-					<div class="rounded-full w-[20vw] h-[20vw] border border-sandrift-400"></div>
+<div class="fixed -bottom-[50vh] -right-[50vh] md:-bottom-[50vw] md:-right-[50vw] -z-10">
+	<div
+		class="rounded-full w-[100vh] h-[100vh] p-[10vh] md:w-[100vw] md:h-[100vw] md:p-[10vw] border border-sandrift-400/20"
+	>
+		<div
+			class="rounded-full w-[80vh] h-[80vh] p-[10vh] md:w-[80vw] md:h-[80vw] md:p-[10vw] border border-sandrift-400/40"
+		>
+			<div
+				class="rounded-full w-[60vh] h-[60vh] p-[10vh] md:w-[60vw] md:h-[60vw] md:p-[10vw] border border-sandrift-400/60"
+			>
+				<div
+					class="rounded-full w-[40vh] h-[40vh] p-[10vh] md:w-[40vw] md:h-[40vw] md:p-[10vw] border border-sandrift-400/80"
+				>
+					<div
+						class="rounded-full w-[20vh] h-[20vh] md:w-[20vw] md:h-[20vw] border border-sandrift-400"
+					></div>
 				</div>
 			</div>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1 class="font-display">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { HugeiconsIcon } from '@hugeicons/svelte';
+	import { CodeSquareIcon, Mail02Icon, WorkHistoryIcon } from '@hugeicons-pro/core-solid-rounded';
+</script>
+
+<div class="space-y-6">
+	<div class="space-y-2">
+		<h1 class="h1">Emma den Engelsen</h1>
+		<p>
+			I make and break thingies, usually on the internet. I like messing with software, modding 3d
+			printers and writing overcomplicated bash scripts on occasion.
+		</p>
+	</div>
+	<div class="flex h-25 gap-2.5">
+		<button
+			class="w-1/3 flex flex-col bg-sandrift-200 hover:bg-sandrift-300/60 active:bg-sandrift-300 active:w-[38%] items-center justify-center gap-0.5 transition-all rounded-l-3xl rounded-r-lg"
+		>
+			<HugeiconsIcon icon={CodeSquareIcon} size={32} color="currentColor" />
+			<span>Projects</span>
+		</button>
+		<button
+			class="w-1/3 flex flex-col bg-sandrift-200 hover:bg-sandrift-300/60 active:bg-sandrift-300 active:w-[38%] items-center justify-center gap-0.5 transition-all rounded-lg"
+		>
+			<HugeiconsIcon icon={WorkHistoryIcon} size={32} color="currentColor" />
+			<span>Experience</span>
+		</button>
+		<button
+			class="w-1/3 flex flex-col bg-sandrift-200 hover:bg-sandrift-300/60 active:bg-sandrift-300 active:w-[38%] items-center justify-center gap-0.5 transition-all rounded-r-3xl rounded-l-lg"
+		>
+			<HugeiconsIcon icon={Mail02Icon} size={32} color="currentColor" />
+			<span>Contact</span>
+		</button>
+	</div>
+	<div class="flex gap-2"></div>
+</div>
