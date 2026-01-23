7e701cfbd36148b990cdd33d58d1a1092f7d1df5
Emma <hello@prpl.wtf>
2026-01-23 16:35:57 +0100

    feat: add srh.dog 88x31

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,8 @@
 		{ src: '/img/88x31/auri_lol.gif', alt: 'auri.lol', href: 'https://auri.lol' },
 		{ src: '/img/88x31/liv_town.gif', alt: 'liv.town', href: 'https://liv.town' },
 		{ src: '/img/88x31/circulars_dev.gif', alt: 'circulars.dev', href: 'https://circulars.dev' },
-		{ src: '/img/88x31/bad_cash.gif', alt: 'bad.cash', href: 'https://bad.cash' }
+		{ src: '/img/88x31/bad_cash.gif', alt: 'bad.cash', href: 'https://bad.cash' },
+		{ src: '/img/88x31/srh_dog.gif', alt: 'srh.dog', href: 'https://srh.dog' }
 	];
 </script>
 
