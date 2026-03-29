1605b4cc969331e4de62a8f0d1048eb577cb81fb
Emma <hello@prpl.wtf>
2026-03-29 20:14:02 +0200

    feat: first attempt at switching to github pages

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
