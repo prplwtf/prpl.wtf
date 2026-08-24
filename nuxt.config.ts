138d1b570154df67bc9f7a2be0e240cd1310ee1f
Emma <hello@prpl.wtf>
2026-08-24 12:33:29 +0200

    feat: file tree stuffs

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/fonts'],
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
