cb86371707345f643d7c4a9dc3ae332a5e34841d
Emma <hello@prpl.wtf>
2026-08-31 18:09:11 +0200

    feat: even more banner shenanigans its coming together

--- app/pages/works/index.vue
+++ app/pages/works/index.vue
@@ -1,62 +1,64 @@
 <template>
-  <div class="group/list flex h-full w-full items-stretch">
-    <div
-      v-for="project in projects"
-      :key="project.name"
-      :class="[
-        'group/container min-w-0 flex-1 pr-4 transition-[flex] duration-300 ease-in-out last:pr-0',
-        'md:group-has-[&:hover]/list:flex-[0.65] md:group-has-[&:hover]/list:hover:flex-[1.35]',
-        'lg:group-has-[&:hover]/list:flex-[0.8] lg:group-has-[&:hover]/list:hover:flex-[1.2]',
-        'xl:group-has-[&:hover]/list:flex-[0.9] xl:group-has-[&:hover]/list:hover:flex-[1.1]',
-      ]"
-    >
+  <div class="h-full w-full max-lg:bg-mist-900 max-lg:p-4">
+    <div class="group/list flex h-full w-full items-stretch max-lg:flex-col">
       <div
-        class="relative h-full w-full overflow-hidden rounded-2xl bg-mist-900"
+        v-for="project in projects"
+        :key="project.name"
+        :class="[
+          'group/container min-w-0 flex-1 transition-[flex] duration-300 ease-in-out last:pr-0 lg:pr-4',
+          'md:group-has-[&:hover]/list:flex-[0.65] md:group-has-[&:hover]/list:hover:flex-[1.35]',
+          'lg:group-has-[&:hover]/list:flex-[0.8] lg:group-has-[&:hover]/list:hover:flex-[1.2]',
+          'xl:group-has-[&:hover]/list:flex-[0.9] xl:group-has-[&:hover]/list:hover:flex-[1.1]',
+        ]"
       >
-        <!-- background image cover -->
-        <NuxtImg
-          class="absolute top-0 left-0 z-10 h-full w-full object-cover"
-          :src="project.cover"
-        />
-
-        <!-- background gradient -->
         <div
-          class="absolute top-1/2 left-0 z-20 h-full w-full bg-linear-to-b from-transparent to-black/40 opacity-50 transition-all duration-300 group-hover/container:top-0 group-hover/container:opacity-100"
-        />
-
-        <!-- project logo -->
-        <div class="absolute top-0 left-0 z-30 h-full w-full">
-          <div class="flex h-full flex-row items-center justify-center">
-            <NuxtImg :src="project.logo" class="w-5/6 max-w-75 xl:w-2/3" />
-          </div>
-        </div>
-
-        <!-- link icon -->
-        <div class="absolute top-0 right-0 z-40 p-6">
-          <Icon
-            name="bi:arrow-up-right-square"
-            :size="48"
-            mode="svg"
-            class="opacity-100 transition-opacity duration-300 group-hover/container:opacity-0"
+          class="relative h-full w-full overflow-hidden rounded-2xl bg-mist-900"
+        >
+          <!-- background image cover -->
+          <NuxtImg
+            class="absolute top-0 left-0 z-10 h-full w-full object-cover"
+            :src="project.cover"
           />
-        </div>
-        <div class="absolute top-0 right-0 z-45 p-6">
-          <Icon
-            name="bi:arrow-up-right-square-fill"
-            :size="48"
-            mode="svg"
-            class="opacity-0 transition-opacity duration-300 group-hover/container:opacity-100"
-          />
-        </div>
 
-        <!-- project info -->
-        <div class="absolute top-0 left-0 z-50 h-full w-full p-6">
+          <!-- background gradient -->
           <div
-            class="flex h-full w-full flex-col items-start justify-end overflow-hidden"
-          >
-            <div class="space-y-1">
-              <h2 class="max-w-full truncate italic">{{ project.name }}</h2>
-              <p class="max-w-full truncate">{{ project.summary }}</p>
+            class="absolute top-1/2 left-0 z-20 h-full w-full bg-linear-to-b from-transparent to-black/40 opacity-50 transition-all duration-300 group-hover/container:top-0 group-hover/container:opacity-100"
+          />
+
+          <!-- project logo -->
+          <div class="absolute top-0 left-0 z-30 h-full w-full">
+            <div class="flex h-full flex-row items-center justify-center">
+              <NuxtImg :src="project.logo" class="w-5/6 max-w-75 xl:w-2/3" />
+            </div>
+          </div>
+
+          <!-- link icon -->
+          <div class="absolute top-0 right-0 z-40 p-6">
+            <Icon
+              name="bi:arrow-up-right-square"
+              :size="48"
+              mode="svg"
+              class="opacity-100 transition-opacity duration-300 group-hover/container:opacity-0"
+            />
+          </div>
+          <div class="absolute top-0 right-0 z-45 p-6">
+            <Icon
+              name="bi:arrow-up-right-square-fill"
+              :size="48"
+              mode="svg"
+              class="opacity-0 transition-opacity duration-300 group-hover/container:opacity-100"
+            />
+          </div>
+
+          <!-- project info -->
+          <div class="absolute top-0 left-0 z-50 h-full w-full p-6">
+            <div
+              class="flex h-full w-full flex-col items-start justify-end overflow-hidden"
+            >
+              <div class="space-y-1">
+                <h2 class="max-w-full truncate italic">{{ project.name }}</h2>
+                <p class="max-w-full truncate">{{ project.summary }}</p>
+              </div>
             </div>
           </div>
         </div>
@@ -77,25 +79,25 @@ const projects = [
     name: 'Nebula',
     summary: 'Pterodactyl theme',
     logo: '/img/works/logo/nebula.svg',
-    cover: '/img/placeholders/works/two.webp',
+    cover: '/img/works/banner/vertical/nebula.jpg',
   },
   {
     name: 'Writea',
     summary: 'Blogging site',
     logo: '/img/works/logo/writea.svg',
-    cover: '/img/placeholders/works/three.webp',
+    cover: '/img/works/banner/vertical/writea.jpg',
   },
   {
     name: 'Bashatime',
-    summary: 'Wakatime tracking',
+    summary: 'Wakatime plugin',
     logo: '/img/works/logo/bashatime.svg',
-    cover: '/img/placeholders/works/four.webp',
+    cover: '/img/works/banner/vertical/bashatime.jpg',
   },
   {
     name: 'Circles',
-    summary: 'Brandkit',
+    summary: 'Brand kit',
     logo: '/img/works/logo/circles.svg',
-    cover: '/img/placeholders/works/five.webp',
+    cover: '/img/works/banner/vertical/circles.jpg',
   },
 ]
 </script>
