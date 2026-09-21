acfad999cc4592d5c9451f9bc2094876e89872de
Emma <hello@prpl.wtf>
2026-09-20 20:42:06 -0400

    i hate making markdown components but i love using them

--- app/components/prose/ProsePre.vue
+++ app/components/prose/ProsePre.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="my-4 overflow-auto rounded-2xl bg-mist-800 p-2">
     <div class="flex justify-between pb-2">
-      <div class="flex items-center">
-        <Icon name="simple-icons:javascript" />
+      <div class="flex items-center gap-1.5 ps-1">
+        <Icon name="bi:code" />
         <span>{{ props.filename || props.language || 'code' }}</span>
       </div>
       <button class="rounded-lg bg-mist-700 px-2 py-1">Copy</button>
